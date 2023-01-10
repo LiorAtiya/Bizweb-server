5f34cdbecaf143366a447a3d3917a8ac37d65aad
Lior <lior4007@gmail.com>
2023-01-10 02:06:27 +0200

    remove node moudles

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.get('/', (req, res) => {
     res.send('Hello From server of Facework')
 })
 
-// const port = process.env.PORT || 5015;
-const port = 5015
+const port = process.env.PORT || 5015;
+// const port = 5015
 app.listen(port, () => {
     console.log("Server Started with http://localhost:" + port + "/");
 })
