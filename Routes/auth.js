09b8d1e0e2de6845ad01414a15d6178edc9f59f6
Lior <lior4007@gmail.com>
2023-07-04 02:30:41 +0300

    .

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../Models/userDetails')
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = "fdsfdsdcdswere()fdsfds32423fscdsf343fdfdfdfxasdggg"
+const nodemailer = require('nodemailer');
 
 //Register new user
 router.post('/register', async (req, res) => {
@@ -62,7 +63,7 @@ router.post('/login', async (req, res) => {
 router.post('/fast-login', async (req, res) => {
     const { firstname, lastname, username,
         email } = req.body;
-    
+
     try {
         //checks if the user exist in database
         const user = await User.findOne({ 'email': email });
@@ -89,5 +90,94 @@ router.post('/fast-login', async (req, res) => {
         res.status(500).json(error);
     }
 
+})
+
+router.post('/forgot-password', async (req, res) => {
+    const { email } = req.body
+    try {
+        const oldUser = await User.findOne({ email });
+        if (!oldUser) {
+            return res.sendStatus(404)
+        }
+        const secret = JWT_SECRET + oldUser.password
+        const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+            expiresIn: '5m',
+        })
+
+        const link = `https://bizweb-israel.netlify.app/resetpassword/${oldUser._id}/${token}}`;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host:'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'bizwebisrael@gmail.com',
+                pass: 'smihyerujkuurmwe'
+            }
+        });
+
+        const info = await transporter.sendMail({
+            from: {
+                name: 'Bizweb Israel',
+                address:'bizwebisrael@gmail.com'
+            }, // sender address
+            to: [email], // list of receivers
+            subject: "Password Reset", // Subject line
+            text: `Enter the password reset link \n${link}`, // plain text body
+        });
+
+        return res.sendStatus(200)
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
+})
+
+// router.get('/reset-password/:id/:token', async (req, res) => {
+//     const { id, token } = req.params;
+//     const oldUser = await User.findOne({ _id: id })
+//     if (!oldUser) {
+//         return res.sendStatus(404)
+//     }
+//     const secret = JWT_SECRET + oldUser.password
+//     try {
+//         const verify = jwt.verify(token, secret);
+//         res.render('index', { email: verify.email, status: 'Not Verified' })
+
+//     } catch (error) {
+//         return res.send('Not Verified')
+
+//     }
+// })
+
+router.post('/reset-password/:id/:token', async (req, res) => {
+    const { id, token } = req.params;
+    const { password } = req.body;
+    const oldUser = await User.findOne({ _id: id })
+    if (!oldUser) {
+        return res.sendStatus(404)
+    }
+    const secret = JWT_SECRET + oldUser.password
+    try {
+        const verify = jwt.verify(token, secret);
+        const encrypedPassword = await bcrypt.hash(password, 10)
+        await User.updateOne(
+            {
+                _id: id,
+            },
+            {
+                $set: {
+                    password: encrypedPassword
+                }
+            }
+        );
+        return res.sendStatus(200)
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
 })
 module.exports = router;
